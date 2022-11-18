7ea8f422f4a0422644e35266f67a7b7d15d58e68
kaancakr <erenkaancakr@gmail.com>
2022-11-18 11:13:22 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.*;
+import javax.swing.JFrame;
+
+
+public class Main {
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        JFrame obj=new JFrame();
+        Gameplay gamePlay = new Gameplay();
+
+        obj.setBounds(10, 10, 1366, 768);
+        obj.setTitle("SHOOT TO SURVIVE");
+        obj.setBackground(Color.gray);
+        obj.setResizable(false);
+
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+        obj.setVisible(true);
+
+        File file = new File("action_time.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        clip.start();
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+
+    }
+
+}
