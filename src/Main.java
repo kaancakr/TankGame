f845080c3dc16b8603a9f2a97ffbd0c32baa300d
kaancakr <erenkaancakr@gmail.com>
2022-11-18 21:25:48 +0300

    FiLE NAME CHANGED

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         Gameplay gamePlay = new Gameplay();
 
         obj.setBounds(10, 10, 1366, 768);
-        obj.setTitle("SHOOT AND RN");
+        obj.setTitle("SHOOT AND RUN");
         obj.setBackground(Color.gray);
         obj.setResizable(false);
 
